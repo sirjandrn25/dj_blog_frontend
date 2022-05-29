e861023c47f723c51760057f9c23b5aa142528b4
sirjan tamang <sirjantmg99@gmail.com>
2022-05-29 18:44:47 +0545

    initial setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className=''>
+          <h1 className="text-3xl font-bold underline text-gray-400">
+            Hello world!
+          </h1>
+
+    </div>
+  )
+}
+
+export default App
